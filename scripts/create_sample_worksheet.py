46c4e2a87c6c5f6dbf0b91b1c773cf31bc115213
Tony Lee <tonyh.lee@yahoo.com>
2020-02-15 11:04:37 -0800

    invalid directives

--- scripts/create_sample_worksheet.py
+++ scripts/create_sample_worksheet.py
@@ -69,7 +69,7 @@ class SampleWorksheet:
         self._add_schemas()
         self._add_display_modes()
         self._add_search()
-        # self._add_invalid_directives()
+        self._add_invalid_directives()
         # self._add_rendering_logic()
         self._create_sample_worksheet()
         self._test_worksheet()
