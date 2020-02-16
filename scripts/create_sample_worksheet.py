ec4c285792d82e5cc644bacb4c0aa6ed67f7593d
Tony Lee <tonyh.lee@yahoo.com>
2020-02-15 23:27:15 -0800

    should work now

--- scripts/create_sample_worksheet.py
+++ scripts/create_sample_worksheet.py
@@ -68,7 +68,7 @@ class SampleWorksheet:
         self._add_bundle_references()
         self._add_schemas()
         self._add_display_modes()
-        #self._add_search()
+        self._add_search()
         self._add_invalid_directives()
         self._add_rendering_logic()
         self._create_sample_worksheet()
@@ -282,13 +282,13 @@ class SampleWorksheet:
 
     def _add_display_modes(self):
         self._add_header('Display Modes')
-        # self._add_subheader('Table')
-        # self._add_line('% display table valid_schema')
-        # self._add_bundles(self._valid_bundles)
-        # self._add_table_pattern(
-        #     ['uuid', 'name', 'summary', 'metadata', 'permission', 'group_permissions'],
-        #     len(self._valid_bundles),
-        # )
+        self._add_subheader('Table')
+        self._add_line('% display table valid_schema')
+        self._add_bundles(self._valid_bundles)
+        self._add_table_pattern(
+            ['uuid', 'name', 'summary', 'metadata', 'permission', 'group_permissions'],
+            len(self._valid_bundles),
+        )
 
         self._add_subheader('Image')
         for uuid in self._search_bundles('.png'):
@@ -454,7 +454,7 @@ class SampleWorksheet:
             # When in preview mode, just return the cached UUIDs of valid bundles instead of performing a new search
             return self._valid_bundles
 
-        return run_command(
+        bundles = run_command(
             [
                 self._cl,
                 'search',
@@ -463,7 +463,10 @@ class SampleWorksheet:
                 'created=.sort-',
                 '--uuid-only',
             ]
-        ).split('\n')
+        )
+        if not bundles:
+            return []
+        return bundles.split('\n')
 
     def _add_table_pattern(self, headers, row_count):
         def add_row_pattern(values):
